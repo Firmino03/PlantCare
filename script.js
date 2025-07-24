52f68904779dc7ac78d8979b9043d961e5893c09
Firmino03 <karolaynefirmino27@gmail.com>
2025-07-24 19:57:51 -0300

    atualização do catálogo e hospedagem de rede social

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function filtrar(tipo) {
     planta.style.display = planta.classList.contains(tipo) ? "block" : "none";
   });
 }
+
+function mostrarTodas() {
+  const todas = document.querySelectorAll(".planta");
+  todas.forEach((planta) => {
+    planta.style.display = "block";
+  });
+}
