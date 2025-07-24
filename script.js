eaad9a34e536fde15d8bc9cafb7cafa9922faf91
Firmino03 <karolaynefirmino27@gmail.com>
2025-07-24 13:25:06 -0300

    primeira versao do site

---
+++ script.js
@@ -0,0 +1,6 @@
+function filtrar(tipo) {
+  const todas = document.querySelectorAll(".planta");
+  todas.forEach((planta) => {
+    planta.style.display = planta.classList.contains(tipo) ? "block" : "none";
+  });
+}
